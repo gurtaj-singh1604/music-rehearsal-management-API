ddefcdcc9f7eb4429d4d102739af7e56e5599871
Gurtaj singh <gsingh181@academic.rrc.ca>
2026-04-07 09:05:52 -0500

    Initialized Node TypeScript Express project setup

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+const { createDefaultPreset } = require("ts-jest");
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import('jest').Config} */
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    ...tsJestTransformCfg,
+  },
+  roots: ["<rootDir>/test"],
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.ts"],
+  coverageDirectory: "coverage",
+  moduleFileExtensions: ["ts", "js", "json"],
+};
---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.status(200).json({
+    message: "Music Rehearsal Management API is running",
+  });
+});
+
+export default app;
